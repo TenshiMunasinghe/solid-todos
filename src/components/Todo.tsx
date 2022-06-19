2114ff79d5b5fb9616f92fdf5db905b16d9679c0
TenshiMunasinghe <shadowghost1007@gmail.com>
2022-06-19 18:56:53 +0900

    add basic edit feature

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -6,16 +6,31 @@ interface Props {
 }
 
 const Todo = (props: Props) => {
+  let ref: HTMLInputElement | null = null
   const handleRemove = () => {
     props.removeTodo(props.todo._id)
   }
+
+  const handleEdit = () => {
+    if (ref.value === props.todo.content) return
+    console.log(ref.value)
+  }
+
   return (
     <li class='bg-neutral-800 px-5 py-2 flex items-center space-x-4'>
-      <form class='flex-1 flex items-center space-x-4'>
+      <form
+        onSubmit={e => {
+          e.preventDefault()
+          handleEdit()
+          ref.blur()
+        }}
+        class='flex-1 flex items-center space-x-4'>
         <input
+          ref={ref}
           type='text'
           value={props.todo.content}
-          class='bg-transparent border-0 focus:ring-green-500 flex-1'
+          onBlur={handleEdit}
+          class='bg-transparent border-0 focus:ring-0 focus:bg-neutral-700 flex-1'
         />
         <input
           type='checkbox'
